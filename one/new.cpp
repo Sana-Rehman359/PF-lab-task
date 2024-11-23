b1c1767eb1812a1361f1c5e6d2f508ba2f5e7b87
Sana-Rehman359 <syedasana142004@gmail.com>
2024-11-23 18:27:38 +0500

    Add files via upload

---
+++ one/new.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+
+ string semester;
+string name;
+int age ;
+
+ cout<<"enter your age";
+ cin>>age;
+ cout<<"enter your semester";
+ cin>>semester;
+ cout<<"enter your name";
+ cin>>name;
+ cout<<age;
+ cout<<semester;
+ cout<<name;
+}
---
+++ one/new3.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+string name ;
+ int sem;
+ int sap;
+cout<<"enter your name"<<endl;
+cin>>name;
+cout<<"enter your semsester"<<endl;
+cin>>sem;
+cout<<"enter your sap id"<<endl;
+cin>>sap;
+return 0; 
+}
+
